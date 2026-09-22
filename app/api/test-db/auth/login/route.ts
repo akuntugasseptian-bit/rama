f540e6c8b2d83a62255d4538c4c079b997bdd2ce
rama <akuntugasseptian@gmail.com>
2026-09-22 08:47:22 +0700

    install jsonwebtoken

---
+++ app/api/test-db/auth/login/route.ts
@@ -0,0 +1,3 @@
+import { NextResponse } from "next/server";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken"; 
--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -2,37 +2,37 @@ import mongoose from "mongoose";
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
-if(!MONGODB_URI) {
+if (!MONGODB_URI) {
     throw new Error("MONGODB_URI Belum didefinisikan di .env.local");
 }
 
-interface MongooseCache{
+interface MongooseCache {
     conn: typeof mongoose | null;
     promise: Promise<typeof mongoose> | null;
 }
 
 declare global {
 
-    var mongooseCache: MongooseCache |undefined;
+    var mongooseCache: MongooseCache | undefined;
 }
 
-const cached: MongooseCache = global.mongooseCache || { 
+const cached: MongooseCache = global.mongooseCache || {
     conn: null,
-    promise: null, 
+    promise: null,
 };
 
 global.mongooseCache = cached;
 
-export async function connectDB(){
+export async function connectDB() {
     if (cached.conn) {
         return cached.conn;
     }
-    if (!cached.promise){
-       cached.promise = mongoose.connect(MONGODB_URI!);
+    if (!cached.promise) {
+        cached.promise = mongoose.connect(MONGODB_URI!);
     }
-    try{
+    try {
         cached.conn = await cached.promise;
-    }catch (error){
+    } catch (error) {
         cached.promise = null;
         throw error;
     }
